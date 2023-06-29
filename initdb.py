006366092e33033e84c1d26256d8f7e25eee02d7
jdknightfll <137848591+jdknightfll@users.noreply.github.com>
2023-06-30 02:39:50 +0800

    Create initdb.py

---
+++ initdb.py
@@ -0,0 +1,32 @@
+import os
+import pyautogui
+import socket
+from wy.redistools import *
+from wy.mongotools import *
+
+
+def create_redis_bat(curr_path):
+    run_path = "%s//%s" % (curr_path,"redis.bat")
+    if not os.path.exists(run_path):
+        disk = curr_path[:2]
+        with open("redis.bat","w",encoding="utf-8") as f:
+            f.writelines(disk)
+            f.write("\n")
+            f.writelines("cd %s//redis" % curr_path)
+            f.write("\n")
+            f.writelines("redis-server.exe redis.windows.conf")
+            f.write("\n")
+    return  run_path
+
+def create_mongo_bat(curr_path):
+    run_path = "%s//%s" % (curr_path, "mongo.bat")
+    if not os.path.exists(run_path):
+        disk = curr_path[:2]
+        with open("mongo.bat","w",encoding="utf-8") as f:
+            f.writelines(disk)
+            f.write("\n")
+            f.writelines("cd %s//mongo//bin" % curr_path)
+            f.write("\n")
+            f.writelines("mongod --bind_ip 0.0.0.0 --maxConns 1000 --dbpath  %s\mongo\data\db" % curr_path)
+            f.write("\n")
+    return run_path
