50b9c5d345db5c8829c3f798fd1256fbd7ce219d
jdknightfll <137848591+jdknightfll@users.noreply.github.com>
2023-06-30 02:40:35 +0800

    Update initdb.py

--- initdb.py
+++ initdb.py
@@ -48,3 +48,44 @@ def run_cmd(cmdstr):
     pyautogui.hotkey('winleft', 'r')
     pyautogui.typewrite(cmdstr, 0.001)
     pyautogui.press("enter")
+def isrun_redis(ip):
+    try:
+        myrc = rc(ip, 6379, "mudman1979", 0).getclient()
+        ks = myrc.keys("*")
+        if len(ks) >= 0:
+            flag = True
+            print("redis ok")
+        else:
+            flag = False
+            print("redis no")
+    except:
+        flag = False
+        print("redis no")
+    return flag
+
+def isrun_mongo(ip):
+    try:
+        mc = mongo("mongodb://%s:27017" % ip).getclient("ctg")
+        mc["run_ip"].update({"ip":ip},{"ip":ip},upsert=True)
+        print("mongo ok")
+        flag = True
+    except:
+        flag = False
+        print("mongo no")
+    return flag
+
+
+
+
+def run(curr_path):
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    mongo_cmd = create_mongo_bat(curr_path)
+    create_mongo_client_bat(curr_path)
+    if not isrun_mongo(ip):
+        run_cmd(mongo_cmd)
+    print("mongo run_at: %s:27017" % ip)
+
+
+if __name__ == "__main__":
+    run(os.getcwd())
